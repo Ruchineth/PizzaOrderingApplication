0ddcd104c2eb2b4bf81c04613e556b2bc3d31895
Ruchineth <35953159+Ruchineth@users.noreply.github.com>
2021-08-18 22:12:09 +0530

    pizzaprocessing service

---
+++ ProcessingService/src/test/java/com/ruchineth/ProcessingService/ProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruchineth.ProcessingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
